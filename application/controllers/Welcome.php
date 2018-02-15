e180f354089d1532c3ffa8eb7218c60ff1977a22
wozenwho <wilson.huu@gmail.com>
2018-02-15 12:55:30 -0800

    Completed job uno senor

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,8 +19,37 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+
+		$tasks = $this->tasks->all();   // get all the tasks
+
+		// process the array in reverse, until we have five
+		$count = 0;
+		
+		foreach(array_reverse($tasks) as $task) {
+			$task->priority = $this->app->priority($task->priority);
+			$display_tasks[] = (array) $task;
+		$this->data['display_tasks'] = $display_tasks;
+		}
+		
+
+		// count how many are not done
+		$count = 0;
+		foreach($tasks as $task) {
+				if ($task->status != 2) $count++;
+		}
+		// and save that as a view parameter
+		$this->data['remaining_tasks'] = $count;
+
 		$this->data['pagebody'] = 'homepage';
 		$this->render(); 
 	}
 
+	function render($template = 'template')
+	{
+		$this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+		// use layout content if provided
+		if (!isset($this->data['content']))
+			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+		$this->parser->parse($template, $this->data);
+	}
 }
